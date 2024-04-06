e2d05ad4783076d57c8bdcf106fb702f1f11da13
Jan Přindiš <jan.prindis3@gmail.com>
2024-04-07 00:03:02 +0200

    Import cleanup

--- app/src/main/java/com/honz/itsvisualizer/cards/CamCard.kt
+++ app/src/main/java/com/honz/itsvisualizer/cards/CamCard.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import com.honz.itsvisualizer.R
 import utils.storage.data.Cam
 
--- app/src/main/java/com/honz/itsvisualizer/cards/MapemCard.kt
+++ app/src/main/java/com/honz/itsvisualizer/cards/MapemCard.kt
@@ -1,7 +1,6 @@
 package com.honz.itsvisualizer.cards
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
